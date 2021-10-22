404004094f66cca4139b4ef9a36244d98aecfe06
jaeseok-go <gojeasuk2@naver.com>
2021-10-22 15:50:18 +0900

    [ADD] getPortfolio api swagger doc

--- src/main/java/com/example/portfolioservice/config/SwaggerConfig.java
+++ src/main/java/com/example/portfolioservice/config/SwaggerConfig.java
@@ -11,26 +11,29 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
 	 @Bean
 	    public Docket api() {
-	        return new Docket(DocumentationType.OAS_30)
-	                .useDefaultResponseMessages(false)
-	                .select()
-	                .apis(RequestHandlerSelectors.basePackage("com.example.portfolioservice.config"))
-	                .paths(PathSelectors.any())
-	                .build()
-	                .apiInfo(apiInfo());
+		 return new Docket(DocumentationType.OAS_30) // open api spec 3.0
+				 .select()
+				 .apis(RequestHandlerSelectors.any())
+				 .paths(PathSelectors.any())
+				 .build()
+				 .apiInfo(apiInfo());
 	    }
 
 	    
 	    private ApiInfo apiInfo() {
 	        return new ApiInfoBuilder()
-	                .title( "Portfolio Swagger")
-	                .description("portfolio swagger config")
+	                .title( "Portfolio Service Swagger")
+	                .description("MoneyMany의 백엔드 포트폴리오 서비스 API 명세")
 	                .version("1.0")
 	                .build();
 	    }
