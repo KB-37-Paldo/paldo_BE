885ef707935a12bd3f2ec3c31235dc6aacef712c
lsdtve <lsdtve@gmail.com>
2021-10-26 14:08:01 +0900

    [ADD] asset response fix

--- src/main/java/com/example/portfolioservice/model/HoldingsResponseDto.java
+++ src/main/java/com/example/portfolioservice/model/HoldingsResponseDto.java
@@ -3,9 +3,7 @@ package com.example.portfolioservice.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Getter
 @Setter
@@ -13,6 +11,12 @@ public class HoldingsResponseDto {
     private long targetPrice = 0;
     private Date targetDate = null;
     private long totAmount = 0;
+    private float cashAmount;
+    private float stockAmount;
+    private float realAssetsAmount;
+    private float bondAmount;
+    private float fundAmount;
+    private float realEstateAmount;
     private HoldingsDetailDto cash = new HoldingsDetailDto();
     private HoldingsDetailDto stock = new HoldingsDetailDto();
     private HoldingsDetailDto realAssets = new HoldingsDetailDto();
@@ -28,6 +32,12 @@ public class HoldingsResponseDto {
         this.totAmount += this.bond.setTotAmount();
         this.totAmount += this.fund.setTotAmount();
         this.totAmount += this.realEstate.setTotAmount();
+        this.cashAmount = Math.round(((float) this.cash.getTotAmount() / this.totAmount)*1000)/10.0f;
+        this.stockAmount = Math.round(((float) this.stock.getTotAmount() / this.totAmount)*1000)/10.0f;
+        this.realAssetsAmount = Math.round(((float) this.realAssets.getTotAmount() / this.totAmount)*1000)/10.0f;
+        this.bondAmount = Math.round(((float) this.bond.getTotAmount() / this.totAmount)*1000)/10.0f;
+        this.fundAmount = Math.round(((float) this.fund.getTotAmount() / this.totAmount)*1000)/10.0f;
+        this.realEstateAmount = Math.round(((float) this.realEstate.getTotAmount() / this.totAmount)*1000)/10.0f;
     }
 
 }
